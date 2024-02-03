39ce35f4b153a289e7da2d06fbe4c670f74ba8aa
Fig <bethanytrr@gmail.com>
2024-02-03 13:24:47 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const cors = require("cors");
+const express = require("express");
+//const keyPinsController = require('./controllers/keyPinsController')
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use('/keyPins', diceSetController)
+
+app.get("/", (req, res) => {
+  res.status(200).send("Gatekey Home");
+});
+
+
+app.get('*', (req, res) => {
+  res.status(404).send('Error: Invalid Route')
+})
+
+
+module.exports = app;
