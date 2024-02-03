fd48bc756f11f24cd475fc6d1cbf2146bfa80e49
Fig <bethanytrr@gmail.com>
2024-02-03 16:12:00 -0500

    Seed and Schema.sql updated, tables now working

--- db/schema.sql
+++ db/schema.sql
@@ -1,13 +1,11 @@
 DROP DATABASE IF EXISTS gatekey_dev;
 CREATE DATABASE gatekey_dev;
 \c gatekey_dev
-DROP TABLE gatekey;
-CREATE TABLE gatekey
-(
-id SERIAL PRIMARY KEY , 
-name TEXT NOT NULL,
-color TEXT NOT NULL,
-address TEXT NOT NULL,
-comment TEXT NOT NULL,
-)
 
+CREATE TABLE gatekey(
+    id SERIAL PRIMARY KEY, 
+    name TEXT NOT NULL,
+    color TEXT NOT NULL,
+    address TEXT NOT NULL,
+    comment TEXT NOT NULL
+);
